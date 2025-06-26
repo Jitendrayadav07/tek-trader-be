ec4639da1ff1f8372e5f62b481cb2d4af94c8d78
Jitu Yadav <jitendra.bechan.yadav@gmail.com>
2025-06-26 15:05:47 +0530

    recent token and pir api

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,11 @@ const router = express.Router();
 // Import route handlers
 const shillCategoryRoutes = require("./shillCategoryRoutes");
 const shillBoardRoutes = require("./shillBoardRoutes");
+const tokenRoutes = require("./token");
 
 router.use("/shill_category",shillCategoryRoutes);
 router.use("/shill_board",shillBoardRoutes);
+router.use("/tokens",tokenRoutes);
+
 
 module.exports = router;
